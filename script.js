97d3586a2e8c6d2756487854b4f76bea2971f851
jordan <jpolk@atl.yearup.org>
2017-04-06 17:13:59 -0400

    changed a bunch of shit

--- script.js
+++ script.js
@@ -3,49 +3,55 @@ function test( ) {
 	console.log("Hello Me!");
 }
 test();
-$(document).ready(function(){
-	$("span").click(function(){
-		$(this).html("jQuery is working.");
-	});
 
 //jQuery************************************//
 
 
 
 //Javascript********************************//
-var string = prompt('Enter 5 numbers separated by a comma.')
-console.log(string);
-var res = string.split(",");
-console.log(res);
+$(document).ready(function() {
+		$('input:radio[name=aOM]').change(function () {
+				function convertToInteger() {
+		var integer = [];
+		var input = document.getElementById("input").value;
+		console.log(input + " fart");
+		var res = input.split(",");
+		console.log(res + " phlbbbtt");
 
-var integer = [];
+		for (var i = 0; i < res.length; i++) {
+			var int = parseInt(res[i].valueOf());
+			console.log(int);
+			integer.push(int);
+		}
+		console.log(integer);
 
-for (var i = 0; i < res.length; i++) {
-	var int = parseInt(res[i].valueOf());
-	console.log(int);
-	integer.push(int);
-}
-console.log(integer);
 
-function max(integer) {
-    var m = -Infinity, i = 0, n = integer.length;
-    for (; i != n; ++i) {
-        if (integer[i] > m) {
-            m = integer[i];
-        }
-    }
-    return m;
-}
+		function max(integer) {
+    		var m = -Infinity, i = 0, n = integer.length;
+    		for (; i != n; ++i) {
+        		if (integer[i] > m) {
+            		m = integer[i];
+        		}
+    		}
+    		return m;
+		}; console.log(max(integer) + " Is the Max.");
 //Math.max.apply(null, integer)
-console.log(max(integer) + " Is the largest number in the set.");
 
-function avg(integer) {
-var total = 0;
-	for(var i = 0; i < integer.length; i++) {
-    total += integer[i];
-	}
-	var avg = total / integer.length;
-	return avg;
-}
-console.log(avg(integer) + " Is the average of the set.")
-});
+		function avg(integer) {
+		var total = 0;
+			for(var i = 0; i < integer.length; i++) {
+    		total += integer[i];
+			}
+			var avg = total / integer.length;
+			return avg;
+		};console.log(avg(integer) + " Is the Avg.");
+	};
+
+				if (this.value == 'average') {
+					$("#output").innerHTML(avg(integer));
+				}
+				else if (this.value == 'max') {
+					$("#output").innerHTML(max(integer));
+				}
+		})
+	});
